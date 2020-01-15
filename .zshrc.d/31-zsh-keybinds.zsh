d529e1728b134078c782a24d67fe2c3dd85ba85b
McModder <mcmodder@mcmodder.ru>
2020-01-15 21:54:29 +0300

    more oh-my-zsh keybinds

--- .zshrc.d/31-zsh-keybinds.zsh
+++ .zshrc.d/31-zsh-keybinds.zsh
@@ -1,8 +1,18 @@
+## From OH-MY-ZSH https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
+
 # start typing + [Up-Arrow] - fuzzy find history forward
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search
+bindkey '^[[A' up-line-or-beginning-search
 # start typing + [Down-Arrow] - fuzzy find history backward
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "^[[B" down-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search
+
+# [Shift-Tab] - move through the completion menu backwards
+bindkey '^[[Z' reverse-menu-complete
+
+# [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5C' forward-word
+# [Ctrl-LeftArrow] - move backward one word
+bindkey '^[[1;5D' backward-word
