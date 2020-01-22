7db3c1150e975e8a4693d4f6a90cb6ae6ebdf35b
McModder <mcmodder@mcmodder.ru>
2020-01-22 12:46:08 +0300

    more aliases

--- .zshrc.d/90-aliases.zsh
+++ .zshrc.d/90-aliases.zsh
@@ -11,5 +11,14 @@ function nb {
     xdg-open ./build/libs/
 }
 
+function nt {
+    xdg-open "$(t)"
+}
+
 alias dns='resolvectl query'
 alias mkdir='mkdir -p'
+
+function mkcdir {
+    mkdir -p "$1"
+    cd "$1"
+}
