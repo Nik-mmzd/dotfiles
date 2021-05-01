7ef758d1c2cf33b23a7b69971c1cc23a9060d647
McModder <mcmodder@mcmodder.ru>
2021-05-01 18:37:13 +0300

    compinit for asdf

--- .zshrc.d/71-asdf.zsh
+++ .zshrc.d/71-asdf.zsh
@@ -1,4 +1,7 @@
 export ASDF_DIR="${ASDF_DIR:-$HOME/.local/share/asdf}"
 export ASDF_DATA_DIR="$ASDF_DIR"
 export ASDF_CONFIG_FILE="$ASDF_DIR/.asdfrc"
-[[ -s "$ASDF_DIR/asdf.sh" ]] && source "$ASDF_DIR/asdf.sh" && fpath=(${ASDF_DIR}/completions $fpath)
+if [[ -s "$ASDF_DIR/asdf.sh" ]]; then
+  source "$ASDF_DIR/asdf.sh"
+  fpath=(${ASDF_DIR}/completions $fpath)
+fi
