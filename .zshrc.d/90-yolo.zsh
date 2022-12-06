4cec29fee5c91a330331c10c8ef3bc8ad5990ab6
McModder <mcmodder@mcmodder.ru>
2022-12-06 18:02:10 +0300

    fix yolo

--- .zshrc.d/90-yolo.zsh
+++ .zshrc.d/90-yolo.zsh
@@ -1,6 +1,6 @@
 alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'
 alias zplugrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zplugrc'
-alias yolo='git commit -m "$(curl -s whatthecommit.com/index.txt)"'
+alias yolo='git commit -m "$(curl -s https://whatthecommit.com/index.txt)"'
 
 [[ "${ZDOTDIR:-$HOME}" != "$HOME" ]] && function rc {
   case "$1" in
