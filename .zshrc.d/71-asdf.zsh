697a3b866da94e63e676ca176c26b5daa4cf6bd7
McModder <me@modder.pw>
2025-03-20 03:33:31 +0300

    asdf 0.16.0 support

--- .zshrc.d/71-asdf.zsh
+++ .zshrc.d/71-asdf.zsh
@@ -1,7 +1,13 @@
 export ASDF_DIR="${ASDF_DIR:-$HOME/.local/share/asdf}"
 export ASDF_DATA_DIR="$ASDF_DIR"
 export ASDF_CONFIG_FILE="$ASDF_DIR/.asdfrc"
-if [[ -s "$ASDF_DIR/asdf.sh" ]]; then
+
+if command -v asdf &> /dev/null; then
+  export PATH="${ASDF_DATA_DIR}/shims:$PATH"
+  if [[ -d "${ASDF_DIR}/completions" ]]; then
+    fpath=(${ASDF_DIR}/completions $fpath)
+  fi
+elif [[ -s "$ASDF_DIR/asdf.sh" ]]; then
   source "$ASDF_DIR/asdf.sh"
   fpath=(${ASDF_DIR}/completions $fpath)
 fi
